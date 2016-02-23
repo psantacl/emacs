0f5127cc28c0d5c7141c29e11ac2e7efc13dc628
Paul Santa Clara <psantaclara@rjmetrics.com>
2016-02-22 20:25:19 -0500

    jhost snippet

---
+++ .emacs.d/snippets/clojure-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `clojure-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'clojure-mode
+                     '(("jhost" "\n(.getHostName (java.net.InetAddress/getLocalHost)) $0\n" "jhost" nil nil nil "/Users/psantaclara/.emacs.d/snippets/clojure-mode/jhost" nil nil)))
+
+
+;;; Do not edit! File generated at Mon Feb 22 20:23:44 2016
